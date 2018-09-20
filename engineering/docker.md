b9e007ce4c308b2deb1137e38a8161750b76d788
oswystan <oswystan@126.com>
2018-09-20 21:04:56 +0800

    nothing

--- engineering/docker.md
+++ engineering/docker.md
@@ -1,5 +1,10 @@
-# how could I use docker for devops
+# Best practices of dockerfile
 
 - use docker for your build environment: `product_name/build`
 - use docker for your prod test environment: `product_name/prd`
+- Add `ENTRYPOINT` for the default command executed (typically: `/bin/bash`)
+- set `WORKDIR` for the docker image
+- `expose` the ports that needed by others
+- write a shell script for app-releated operations.
+- 
 
---
+++ golang/sdp.go
@@ -0,0 +1,64 @@
+// add()/get()/set()/del()
+// parse()/serialize()
+
+sdp.V
+sdp.O
+sdp.S
+sdp.I
+sdp.U
+sdp.E
+sdp.P
+sdp.C
+sdp.B
+sdp.Z
+sdp.K
+sdp.T
+sdp.R
+sdp.M
+sdp.A
+
+type SdpNode struct {
+    children interface{}
+}
+
+type M struct {
+    type string
+    port uint16
+    protocol string
+    payloadtypes []int
+}
+
+sdp.Parse(str) {
+    ret := new(SdpNode);
+    ret.parse(str);
+    return ret;
+}
+
+sdp.Serialize(node) {
+    return node.serialize();
+}
+
+sdp.New() {
+    return new(SdpNode);
+}
+
+node := sdp.New();
+
+node.Add("m").(sdp.M).Type("audio").Port(111).Protocol("UDP/DTLS").PayloadTypes([111, 98, 100])
+node.M("audio").Add("a").(sdp.A).Key("rtpmap").Value("100 PCMU/8000");
+node.M("vidio").Add("c").(sdp.C).Net("IN").AddressType("IP4").Address("10.1.1.1")
+
+node.M("audio").(sdp.M).A("ftpmap").Del();
+
+
+
+
+
+
+
+
+
+
+
+
+
